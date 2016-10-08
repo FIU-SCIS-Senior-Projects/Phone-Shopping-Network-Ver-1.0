eac0b02f0211d7d6673b5b0349ac0892d6a99be8
Moksedul Alam <dalam004@fiu.edu>
2016-10-08 01:42:27 -0400

    #129: A view is designed and the business logic for more ad information is one

--- PhoneShopping/app/src/main/java/com/socialmobile/phoneshopping/fragments/AdDetailsFragment.java
+++ PhoneShopping/app/src/main/java/com/socialmobile/phoneshopping/fragments/AdDetailsFragment.java
@@ -1,6 +1,13 @@
 package com.socialmobile.phoneshopping.fragments;
 
-import android.support.v4.app.Fragment;
+import android.os.Bundle;
+import android.support.annotation.Nullable;
+import android.view.LayoutInflater;
+import android.view.View;
+import android.view.ViewGroup;
+import android.webkit.WebView;
+
+import com.socialmobile.phoneshopping.R;
 
 /**
  *
@@ -10,4 +17,23 @@ import android.support.v4.app.Fragment;
  */
 
 public class AdDetailsFragment extends FragmentBase<Object> {
+
+    private String data;
+
+    @Override
+    public void onCreate(@Nullable Bundle savedInstanceState) {
+        super.onCreate(savedInstanceState);
+    }
+
+    @Nullable
+    @Override
+    public View onCreateView(LayoutInflater inflater, @Nullable ViewGroup container, @Nullable Bundle savedInstanceState) {
+        return inflater.inflate(R.layout.more_ad_info, container, false);
+    }
+
+    @Override
+    public void onViewCreated(View view, @Nullable Bundle savedInstanceState) {
+        WebView webView = (WebView) view.findViewById(R.id.ad_details_webview);
+        webView.loadUrl("file:///android_asset/addetails/detailed_ad_info.html");
+    }
 }
