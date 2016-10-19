b3d539f1778136bcd536d3df9128ba8c9ac8a29e
Dewan Moksedul Alam <dalam004@fiu.edu>
2016-10-18 20:56:03 -0400

    #140:  initial commit of t he common module intended for the use by both server and the client

---
+++ common/src/main/java/com/socialmobile/phoneshopping/common/model/UserProfile.java
@@ -0,0 +1,10 @@
+package com.socialmobile.phoneshopping.common.model;
+
+
+/**
+ * @author <a href="mailto:dalam004@fiu.edu">Dewan Moksedul Alam</a>
+ * @author last modified by $Author: $
+ * @version $Revision: $ $Date: $
+ */
+public class UserProfile {
+}
