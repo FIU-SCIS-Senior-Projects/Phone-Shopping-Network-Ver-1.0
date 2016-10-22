6e485f92d25003e0ae421a16a14d92d2c359f2a7
Moksedul Alam <dalam004@fiu.edu>
2016-10-22 14:22:13 -0400

    #140: initial architecture of the application. It incorporates Jersey with Spring Framework

---
+++ backend/phoneshopping-webapp/src/main/java/com/socialmobile/phoneshopping/web/spring/ServiceApplication.java
@@ -0,0 +1,15 @@
+package com.socialmobile.phoneshopping.web.spring;
+
+import com.socialmobile.phoneshopping.web.service.resources.UserProfileServiceResource;
+import org.glassfish.jersey.server.ResourceConfig;
+
+/**
+ * @author <a href="mailto:dalam004@fiu.edu">Dewan Moksedul Alam</a>
+ * @author last modified by $Author: $
+ * @version $Revision: $ $Date: $
+ */
+public class ServiceApplication extends ResourceConfig {
+    public ServiceApplication() {
+        packages(true, "com.socialmobile.phoneshopping.web.service.resources");
+    }
+}
