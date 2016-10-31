6f0f7f810135c6c999faf9d2b2d26da4ab81a167
Moksedul Alam <dalam004@fiu.edu>
2016-10-31 05:35:19 -0400

    #142: project dependencies are modified

---
+++ backend/phoneshopping-webapp/src/main/java/com/socialmobile/phoneshopping/service/api/domain/User.java
@@ -0,0 +1,19 @@
+package com.socialmobile.phoneshopping.service.api.domain;
+
+/**
+ * @author <a href="mailto:dalam004@fiu.edu">Dewan Moksedul Alam</a>
+ * @author last modified by $Author: $
+ * @version $Revision: $ $Date: $
+ */
+public class User {
+    private String mUsername;
+
+    private String mFirstName;
+
+    private String mLastName;
+
+    private String mEmail;
+
+    private String mPhone;
+
+}
