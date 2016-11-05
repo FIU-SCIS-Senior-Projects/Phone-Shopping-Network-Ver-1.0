acd9fde2bfb52db7bd9c67240f96ff1876915a16
Moksedul Alam <dalam004@fiu.edu>
2016-11-05 13:48:34 -0400

    #142: reinforce api changes

--- backend/phoneshopping-webapp/src/main/java/com/socialmobile/phoneshopping/service/impl/UserProfileManager.java
+++ backend/phoneshopping-webapp/src/main/java/com/socialmobile/phoneshopping/service/impl/UserProfileManager.java
@@ -3,6 +3,7 @@ package com.socialmobile.phoneshopping.service.impl;
 import com.socialmobile.common.model.UserProfile;
 import com.socialmobile.phoneshopping.service.api.UserProfileService;
 import com.socialmobile.phoneshopping.service.dao.UserProfileDAO;
+import com.socialmobile.phoneshopping.service.domain.User;
 
 import java.util.Optional;
 
@@ -29,21 +30,58 @@ public class UserProfileManager implements UserProfileService {
 
     @Override
     public UserProfile get(final String pIdToGet) {
-        return mUserProfileDAO.get(pIdToGet);
+        User user = mUserProfileDAO.get(pIdToGet);
+        if (user != null) {
+            return userProfileFromUser(user);
+        }
+
+        return null;
     }
 
     @Override
-    public UserProfile create(final Optional<UserProfile> pObjectToCreate) {
-        return mUserProfileDAO.create(pObjectToCreate);
+    public UserProfile create(final UserProfile pObjectToCreate) {
+        User user = userFromUserProfile(pObjectToCreate);
+        user = mUserProfileDAO.create(user);
+        return userProfileFromUser(user);
     }
 
     @Override
-    public UserProfile update(final String pTargetObjectId, final Optional<UserProfile> pUpdateWith) {
-        return mUserProfileDAO.update(pTargetObjectId, pUpdateWith);
+    public UserProfile update(final String pTargetObjectId, final UserProfile pUpdateWith) {
+        User user = userFromUserProfile(pUpdateWith);
+        User updatedUser = mUserProfileDAO.update(pTargetObjectId, user);
+        if (updatedUser != null) {
+            return userProfileFromUser(updatedUser);
+        }
+
+        return null;
     }
 
     @Override
     public boolean delete(final String pIdToDelete) {
         return mUserProfileDAO.delete(pIdToDelete);
     }
+
+    private UserProfile userProfileFromUser(final User pFrom) {
+        UserProfile userProfile =  new UserProfile();
+        userProfile.setUsername(pFrom.getUsername());
+        userProfile.setFirstName(pFrom.getFirstName());
+        userProfile.setLastName(pFrom.getLastName());
+        userProfile.setEmail(pFrom.getEmail());
+        userProfile.setPhone(pFrom.getPhone());
+
+        return userProfile;
+    }
+
+    private User userFromUserProfile(final UserProfile pUser) {
+        User user =  new User();
+        user.setUsername(pUser.getUsername());
+        user.setFirstName(pUser.getFirstName());
+        user.setLastName(pUser.getLastName());
+        user.setEmail(pUser.getEmail());
+        user.setPhone(pUser.getPhone());
+
+        return user;
+    }
+
+
 }
