fb9671504ec19a87428dad12d55c4607069842ba
Dewan Moksedul Alam <dalam004@fiu.edu>
2016-11-08 21:28:06 -0500

    #146: old school testing of the product service

--- backend/phoneshopping-webapp/src/main/java/com/socialmobile/phoneshopping/service/Main.java
+++ backend/phoneshopping-webapp/src/main/java/com/socialmobile/phoneshopping/service/Main.java
@@ -1,12 +1,20 @@
 package com.socialmobile.phoneshopping.service;
 
+import com.fasterxml.jackson.core.JsonProcessingException;
+import com.socialmobile.common.json.JSONObjectFactory;
+import com.socialmobile.common.model.Product;
 import com.socialmobile.common.model.UserProfile;
+import com.socialmobile.phoneshopping.service.api.ProductService;
+import com.socialmobile.phoneshopping.service.impl.ProductManager;
 import com.socialmobile.phoneshopping.service.impl.UserProfileManager;
 import org.apache.commons.dbcp.BasicDataSource;
+import org.springframework.context.ApplicationContext;
 import org.springframework.context.support.ClassPathXmlApplicationContext;
 
 import java.sql.SQLException;
 import java.sql.Statement;
+import java.util.HashMap;
+import java.util.Random;
 
 /**
  * @author <a href="mailto:dalam004@fiu.edu">Dewan Moksedul Alam</a>
@@ -17,6 +25,42 @@ public class Main  {
 
     public static void main(String[] as) {
         ClassPathXmlApplicationContext context = new ClassPathXmlApplicationContext("spring/applicationConfig.xml");
+//        testUserProfileService(context);
+        testProductService(context);
+    }
+
+    private static void testProductService(final ApplicationContext pContext) {
+        ProductService service = pContext.getAutowireCapableBeanFactory().getBean(ProductService.class);
+        System.out.println("Service:: "+service);
+        Product product = createProduct();
+        try {
+            System.out.println(String.format("Product As JSON: (%s)", JSONObjectFactory.getsInstance().objectToString(product)));
+            Product created = service.create(product);
+            System.out.println("Created Product:: "+created.getProductId());
+            created.getAdditionalInfo().put("key4", "value4");
+            Product updated = service.update(created.getProductId(), created);
+            System.out.println(String.format("Is Equals: %s:::: addition info count: %d", (created == updated), updated.getAdditionalInfo().size()));
+            service.delete(updated.getProductId());
+        } catch (JsonProcessingException pE) {
+            pE.printStackTrace();
+        }
+    }
+
+    private static Product createProduct() {
+        Product product = new Product();
+//        product.setProductId(40);
+        product.setTitle("Dummy Product");
+        product.setDescription("This is a Dummy Product");
+        product.setAdditionalInfo(new HashMap<String, String>(){{
+            put("key1", "value1");
+            put("key2", "value2");
+            put("key3", "value3");
+        }});
+        return product;
+    }
+
+    private static void testUserProfileService(ApplicationContext context) {
+
         UserProfileManager manager = (UserProfileManager) context.getBean("userProfileService");
         try {
             UserProfile profile1 = manager.create(getUserProfile("first.last"));
@@ -26,7 +70,6 @@ public class Main  {
             pE.printStackTrace();
         }
         System.out.println("manager:: "+ manager);
-//        System.out.println("Exists: "+manager.exists("first.last"));
     }
 
     private static UserProfile getUserProfile(final String pS) {
